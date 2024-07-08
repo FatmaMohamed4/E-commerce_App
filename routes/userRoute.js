dc3f6b2b9781e1e121e05763872265930821c348
Alaa adel <123251384+Alaaadeln7@users.noreply.github.com>
2024-07-08 03:09:42 +0300

    Update userRoute.js

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,5 +1,5 @@
 import express from "express"
-import  { LogIn, getUser, register } from "../controllers/userController.js";
+import  { LogIn, getUser, register ,updateAccount} from "../controllers/userController.js";
 
 const userRoute = express.Router();
 
@@ -7,4 +7,5 @@ const userRoute = express.Router();
 userRoute.post('/register',register)
 userRoute.post('/login',LogIn)
 userRoute.get('/get', getUser)
+userRoute.patch('/:userId', updateAccount)
 export default userRoute
